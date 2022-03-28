94e483c613f1822945b306d4bc713e51d29884c8
Chris Patti <feoh@feoh.org>
2022-03-27 22:19:04 -0400

    Trap timeout exceptions so the download keeps chugging.
    We should REALLY be a lot smarter about this but given that the client doesn't actually download files it's already gotten I feel like it's not too much of an imposition to do what we're doing.

--- omnia_atari.py
+++ omnia_atari.py
@@ -1,5 +1,8 @@
 from internetarchive import search_items,Item
 from pathlib import Path
+from urllib3.exceptions import ConnectTimeoutError
+from requests.exceptions import ConnectTimeout
+import time
 
 
 DESTINATION_FOLDER_PATH = Path.home() / "iadownloads"
@@ -8,8 +11,19 @@ DESTINATION_FOLDER_PATH.mkdir(exist_ok=True)
 # This is my current best guess to get all 8 bit Atari Binaries the Internet Archive has on offer.
 search = search_items('atr_ AND a8b_ AND Atari_8_bit')
 
-item: Item
-for item in search.iter_as_items():
-    print(f"Downloading {item}")
-    item.download(verbose=True, destdir=str(DESTINATION_FOLDER_PATH))
+# This is imperfect to say the least but if we fail, pause a minute and check the list for uncompleted downloads
+while True:
+    item: Item
+    for item in search.iter_as_items():
+        try:
+            item.download(verbose=True, retries=50, destdir=str(DESTINATION_FOLDER_PATH))
+        except ConnectTimeoutError:
+            print("If at first we don't succeed - START AGAIN! - urllib3 timeout.")
+            time.sleep(30)
+            break
+        except ConnectTimeout:
+            print("If at first we don't succeed - START AGAIN! - requests timeout.")
+            time.sleep(30)
+            break
+
 
